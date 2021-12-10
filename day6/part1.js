70b515052eec2d01b2530897f6453dd7c8a5ec44
Antoni Silvestrovič <antoni.silvestrovic@gmail.com>
2021-12-10 19:52:25 +0200

    Added day 6

--- day6/part1.js
+++ day6/part1.js
@@ -1,4 +1,42 @@
 const fs = require("fs");
 
 const rawFile = fs.readFileSync("./raw.txt", "utf-8");
-const lines = rawFile.split("\n");
+const rawNumbers = rawFile.split(",").map((n) => parseInt(n));
+
+const createNewFish = (initialValue) => {
+  return {
+    ...{
+      value: initialValue,
+      step: function () {
+        this.value--;
+
+        if (this.value < 0) {
+          this.value = 6;
+          return true;
+        }
+
+        return false;
+      },
+    },
+  };
+};
+
+const fishes = rawNumbers.map(createNewFish);
+
+let t = 0;
+while (t < 80) {
+  // console.log("Day ${t}:", fishes.map(({ value }) => value).join());
+
+  const oldFishLength = fishes.length;
+  for (let i = 0; i < oldFishLength; i++) {
+    const didSpawnNewFish = fishes[i].step();
+
+    if (didSpawnNewFish) {
+      fishes.push(createNewFish(8));
+    }
+  }
+
+  t++;
+}
+
+console.log("Fishes after 80 days:", fishes.length);
--- day6/part2.js
+++ day6/part2.js
@@ -1,4 +1,50 @@
 const fs = require("fs");
 
 const rawFile = fs.readFileSync("./raw.txt", "utf-8");
-const lines = rawFile.split("\n");
+const rawNumbers = rawFile.split(",").map((n) => parseInt(n));
+
+let fishes = rawNumbers.map((v) => Object.assign({ count: 1, value: v }));
+
+let t = 0;
+while (t < 256) {
+  console.log("Day:", t);
+  // console.log("Day ${t}:", fishes.map(({ value }) => value).join());
+
+  // Optimise
+  const sortedFishes = fishes.sort((a, b) => a.value - b.value);
+  const optimisedFishes = [];
+  for (const fish of sortedFishes) {
+    if (!optimisedFishes.length) {
+      optimisedFishes.push(fish);
+    } else if (
+      optimisedFishes[optimisedFishes.length - 1].value === fish.value
+    ) {
+      optimisedFishes[optimisedFishes.length - 1].count += fish.count;
+    } else {
+      optimisedFishes.push(fish);
+    }
+  }
+
+  fishes = optimisedFishes;
+
+  // console.log(fishes);
+
+  // 2.
+  const oldFishLength = fishes.length;
+  for (let i = 0; i < oldFishLength; i++) {
+    fishes[i].value--;
+    if (fishes[i].value < 0) {
+      fishes[i].value = 6;
+
+      //
+      fishes.push(Object.assign({ count: fishes[i].count, value: 8 }));
+    }
+  }
+
+  t++;
+}
+
+console.log(
+  "Fishes after 256 days:",
+  fishes.map((obj) => obj.count).reduce((prev, curr) => prev + curr, 0)
+);
